8d3080e3b19e1a9fdf5d66b996fde139ee5a583a
Sakura286 <sakura286@outlook.com>
2025-03-12 15:34:18 +0800

    rocm-llvm

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/llvm-project-rocm/issues
+# Bug-Submit: https://github.com/<user>/llvm-project-rocm/issues/new
+# Changelog: https://github.com/<user>/llvm-project-rocm/blob/master/CHANGES
+# Documentation: https://github.com/<user>/llvm-project-rocm/wiki
+# Repository-Browse: https://github.com/<user>/llvm-project-rocm
+# Repository: https://github.com/<user>/llvm-project-rocm.git
